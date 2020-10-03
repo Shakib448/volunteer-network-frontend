a4e8b8b89b3672c12a5c85a6c8f2409339c163b8
Shakib448 <shakiba448@gmail.com>
2020-10-03 12:34:42 +0600

    Data maped

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import "./Home.css";
+import volunteerData from "../FakeData/FakeData";
 
 const Home = () => {
   return (
@@ -21,7 +22,7 @@ const Home = () => {
                   <Form.Control
                     className="mb-2 home__input"
                     id="inlineFormInput"
-                    placeholder="Jane Doe"
+                    placeholder="Search...."
                   />
                 </Col>
 
@@ -33,6 +34,29 @@ const Home = () => {
               </Form.Row>
             </Form>
           </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          {volunteerData.map(({ title, img, id, color }) => (
+            <Col md={3} key={id}>
+              <Card
+                style={{ width: "16rem", textAlign: "center", border: "none" }}
+              >
+                <Card.Img variant="top" src={img} />
+                <Card.Body
+                  style={{
+                    margin: "-10px 0 10px 0",
+                    backgroundColor: `${color}`,
+                    color: "white",
+                    borderRadius: "0px 0px 10px 10px",
+                  }}
+                >
+                  <Card.Title className="home__title">{title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
