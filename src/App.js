24b01114a63851a6879180067367f2ee85d2dff8
Shakib448 <shakiba448@gmail.com>
2020-10-03 08:47:50 +0600

    Fake Data added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return <div className="App"></div>;
---
+++ src/components/FakeData/FakeData.js
@@ -0,0 +1,55 @@
+import img1 from "../Resource/images/childSupport.png";
+import img2 from "../Resource/images/refuseShelter.png";
+import img3 from "../Resource/images/foodCharity.png";
+import img4 from "../Resource/images/clothSwap.png";
+import img5 from "../Resource/images/riverClean.png";
+import img6 from "../Resource/images/cleanWater.png";
+import img7 from "../Resource/images/goodEducation.png";
+import img8 from "../Resource/images/newBooks.png";
+import img9 from "../Resource/images/studyGroup.png";
+import img10 from "../Resource/images/birdHouse.png";
+import img11 from "../Resource/images/libraryBooks.png";
+import img12 from "../Resource/images/driveSafety.png";
+import img13 from "../Resource/images/musicLessons.png";
+import img14 from "../Resource/images/voteRegister.png";
+import img15 from "../Resource/images/clearnPark.png";
+import img16 from "../Resource/images/ITHelp.png";
+import img17 from "../Resource/images/animalShelter.png";
+import img18 from "../Resource/images/babySit.png";
+import img19 from "../Resource/images/stuffedAnimals.png";
+import img20 from "../Resource/images/schoolSuffiles.png";
+
+const volunteerData = [
+  { id: 1, title: "Child Support", img: img1, color: "" },
+  { id: 2, title: "Refuge Shelter", img: img2, color: "" },
+  { id: 3, title: "Food Charity", img: img3, color: "" },
+  { id: 4, title: "Host A Clothing Swap", img: img4, color: "" },
+  { id: 5, title: "Host A River Clean-Up", img: img5, color: "" },
+  { id: 6, title: "Cleaning Water For Children", img: img6, color: "" },
+  { id: 7, title: "Good Education", img: img7, color: "" },
+  { id: 8, title: "New Books For Children", img: img8, color: "" },
+  { id: 9, title: "Host A Study Group", img: img9, color: "" },
+  {
+    id: 10,
+    title: "Build Bird House For Your Neighbors",
+    img: img10,
+    color: "",
+  },
+  { id: 11, title: "Organize Books At The Library", img: img11, color: "" },
+  { id: 12, title: "Give A Seminar On Driving Safety", img: img12, color: "" },
+  { id: 13, title: "Give Free Music Lesson", img: img13, color: "" },
+  {
+    id: 14,
+    title: "Teach People How To Register To Vote",
+    img: img14,
+    color: "",
+  },
+  { id: 15, title: "Clean Up Your Local Park", img: img15, color: "" },
+  { id: 16, title: "Give It Help To Local Adults", img: img16, color: "" },
+  { id: 17, title: "Foster A Shelter Animal", img: img17, color: "" },
+  { id: 18, title: "Babysit During PTA Meetings", img: img18, color: "" },
+  { id: 19, title: "Collect Stuffed Animals", img: img19, color: "" },
+  { id: 20, title: "Collect School Supplies", img: img20, color: "" },
+];
+
+export default volunteerData;
